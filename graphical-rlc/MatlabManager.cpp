4b5729368f133a1cb0068a1696d2173212bea823
William Blanc <blaksmith57@gmail.com>
2020-04-22 23:47:46 -0400

    Fixes release version not working.
    Problem was caused from std::vector trying to initialize a base class

--- graphical-rlc/MatlabManager.cpp
+++ graphical-rlc/MatlabManager.cpp
@@ -17,32 +17,29 @@ std::vector<matlab::data::Array> MatlabManager::calcTransient(CircuitData* data,
 	matlab::data::ArrayFactory factory;
 	auto path = factory.createCharArray("models/transient");
 	engine->feval(u"addpath", path);
-
-	auto dataVals = data->componentValues();
-	auto config = data->getConfig();
-	auto conditions = data->initialConditions();
-	// Setup the args
-	std::vector<matlab::data::Array> args = {
-		factory.createScalar<double>(dataVals[0].value), // R
-		factory.createScalar<double>(dataVals[1].value), // L
-		factory.createScalar<double>(dataVals[2].value), // C
-		factory.createScalar<double>(conditions[0]), // y_init
-		factory.createScalar<double>(conditions[1]), // dy_init
-		factory.createScalar<double>(conditions[2]), // finalVal
-		factory.createScalar<bool>(config == Circuit::Configuration::SERIES), // isSeries = false if config is parallel
-		factory.createScalar<bool>(data->response == Circuit::Response::STEP), // isStep is false when response is natural
-		factory.createCharArray(outputName)
-	};
-	
-	std::vector<matlab::data::Array> result;
-	// call the function
 	try
 	{
-		result = engine->feval(u"calculate_transient", 5, args, output, error);
+		auto dataVals = data->componentValues();
+		auto config = data->getConfig();
+		auto conditions = data->initialConditions();
+		// Setup the args
+		std::vector<matlab::data::Array> args = {
+			factory.createScalar<double>(dataVals[0].value), // R
+			factory.createScalar<double>(dataVals[1].value), // L
+			factory.createScalar<double>(dataVals[2].value), // C
+			factory.createScalar<double>(conditions[0]), // y_init
+			factory.createScalar<double>(conditions[1]), // dy_init
+			factory.createScalar<double>(conditions[2]), // finalVal
+			factory.createScalar<bool>(config == Circuit::Configuration::SERIES), // isSeries = false if config is parallel
+			factory.createScalar<bool>(data->response == Circuit::Response::STEP), // isStep is false when response is natural
+			factory.createCharArray(outputName)
+		};
+		// call the function
+		return engine->feval(u"calculate_transient", 5, args, output, error);
 	}
 	catch (matlab::engine::EngineException e)
 	{
 		qDebug() << e.what() << "\n";
 	}
-	return result;
+	return {};
 }
